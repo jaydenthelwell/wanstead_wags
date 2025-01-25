93971e46f33e9f61e240c37ba8ace18b48f8d585
jaydenthelwell <jaydenthelwell02@gmail.com>
2025-01-25 15:23:34 +0000

    Fotn size

--- app/javascript/background.js
+++ app/javascript/background.js
@@ -23,7 +23,7 @@ document.addEventListener('turbo:load', function() {
 const observerOptions = {
   root: null,
   rootMargin: '0px',
-  threshold: 0.2
+  threshold: 0.5
 };
 
 function applyStylesForSection(section) {
